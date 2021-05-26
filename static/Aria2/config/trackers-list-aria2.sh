15f271ee379ec2daf3c2ee21c8fa87a8dbbd2fd9
Godcic <Godcic@foxmail.com>
2021-05-27 00:34:58 +0800

    Site updated: 2021-05-27 00:34:13

--- static/Aria2/config/trackers-list-aria2.sh
+++ static/Aria2/config/trackers-list-aria2.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 /etc/init.d/aria2 stop
-ARIA2_CONFIG="/root/.config/aria2/config/aria2.conf"
 list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
-if [ -z "`grep "bt-tracker" $ARIA2_CONFIG`" ]; then
-    sed -i '$a bt-tracker='${list} $ARIA2_CONFIG
+if [ -z "`grep "bt-tracker" /root/.config/aria2/config/aria2.conf`" ]; then
+    sed -i '$a bt-tracker='${list} /root/.config/aria2/config/aria2.conf
     echo add......
 else
-    sed -i "s@bt-tracker.*@bt-tracker=$list@g" $ARIA2_CONFIG
+    sed -i "s@bt-tracker.*@bt-tracker=$list@g" /root/.config/aria2/config/aria2.conf
     echo update......
 fi
 #rm -rf /root/.config/aria2/Download/*
