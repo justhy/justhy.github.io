a313e84462e8799f21f8620a41360cfff640e8ea
Godcic <hy4396@gmail.com>
2024-06-18 23:06:14 +0800

    Site updated: 2024-06-18 23:05:55

--- static/Aria2/config/autoupload.sh
+++ static/Aria2/config/autoupload.sh
@@ -13,14 +13,16 @@ if [ $2 -eq 0 ]
         then
                 exit 0
 fi
+
 while true; do
 filepath=$path
 path=${path%/*}; 
 if [ "$path" = "$downloadpath" ] && [ $2 -eq 1 ]
     then
     ${software} move "$filepath" ${cloudName}:${cloudFolder}/ --onedrive-no-versions --ignore-checksum --ignore-size --ignore-errors --drive-acknowledge-abuse
-	
+
 	exit 0
+	
 elif [ "$path" = "$downloadpath" ]
     then
 	while [[ "`ls -A "$filepath/"`" != "" ]]; do
